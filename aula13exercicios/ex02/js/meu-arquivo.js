d70eb820e5f5f9f29ae23cf347ec87fd7df1253f
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2023-02-26 22:41:32 +0100

    .
    .

---
+++ aula13exercicios/ex02/js/meu-arquivo.js
@@ -0,0 +1,33 @@
+
+
+
+function verificar() {
+
+    // calculando a idade
+    var ano = window.document.getElementById('ano')
+    var idade = new Date().getFullYear() - Number(ano.value)
+
+    // sexo
+    
+    res.innerHTML = `Detectamos SEILA com ${idade} anos (ou quase).`
+    window.document.body.style.backgroundColor = '#6da9b1'
+    
+    if (idade < 3) {
+        img.src = 'imagens/mbebe.jpg'
+    } else if (idade < 14) {
+        img.src = 'imagens/mcrianca.jpg'
+    } else if (idade < 18) {
+        img.src = 'imagens/madolescente.jpg'
+    } else if (idade < 40) {
+        img.src = 'imagens/madulta.jpg'
+    } else if (idade < 65) {
+        img.src = 'imagens/mvelha.jpg'
+    } else if (idade < 110) {
+        img.src = 'imagens/maposentada.jpg'
+    } else {
+        res.innerHTML = `${idade} anos? Já tá no colo do Tinhoso!`
+        img.src = 'imagens/tumulo.jpg'
+    }
+    
+
+}
